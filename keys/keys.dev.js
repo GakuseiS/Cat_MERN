1eedd7091d4328d691ca7c0dec790b57ea27cc17
Ilya <pikabusan@gmail.com>
2021-06-25 16:42:58 +0300

    fix

--- keys/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://IlyaSoma:j8W0KpFSK7BSfZn7@clustercat.ylszk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    secretKey: 'ExpectoPatronum'
-}
